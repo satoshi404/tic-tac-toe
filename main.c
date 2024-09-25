13958181ef8c59356afe86118d99fc4fb87e38a7
satoshi <alwaysindiscipline@gmail.com>
2024-09-25 18:12:07 -0300

    Stack

--- main.c
+++ main.c
@@ -1,26 +1,47 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
+
+
+#define SS 100
+
+void push(int value);
+int pop();
+
+// ------ .rodata
+char stack[SS]       = {0};
+uint8_t p            = {0};
 
 int main(int argc, char* argv) {
-//===========================================      * Initialize registers 
+//===========================================      * Initialize registers
+push(5);
 int eax;
 int ebx;
-int ebc;
-int ebc;
+int ecx;
+int edx;
 goto draw_hash;                                     // draw hash 3 x 3 
 // ==========================================
 _start:                                             // entry point
-loop:
-
-goto loop;
 char buff[4];
-snprintf(buff, 4, "%d\n", eax);
-write(1, buff, 4);                                  // 8 bits * 4 bytes = 32 bits
+//snprintf(buff, 4, "%d", eax);
+buff[0] = 0x30 + eax;
+buff[1] = 0xa;
+write(1, buff, 4);                                  // 8 bits * 5 bytes = 32 bits
 return ebx;                                         // carry 0
 //;;=========================================
 draw_hash: 
-eax = 0x80;                                         // 4 bytes
+eax = pop();                                          // 4 bytes
 ebx ^= ebx;                                         // xor operation
 goto _start;                                        // jmp _start
-} // end program
+} // end main
+// ==========================================
+void push(int value) {
+stack[p] = value;
+p++;                                                // inc pointer
+} // end push
+// ==========================================
+int pop() {
+return stack[--p];
+} // end pop
